faaa71cdf8b857df87e31d3a0f8e05aa0082c824
jeff <solowandererY2K@gmail.com>
2013-03-17 16:40:08 -0700

    Added code to catch PatternSyntaxExceptions.

--- src/sf/filler/tree/BaseTregexFiller.java
+++ src/sf/filler/tree/BaseTregexFiller.java
@@ -3,6 +3,7 @@ package sf.filler.tree;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -22,51 +23,60 @@ import sf.retriever.NerType;
 public abstract class BaseTregexFiller extends Filler {
 	
 	protected List<String> getMatchNames(String pattern, Tree t, Map<String, String> annotations, CorefProvider coref) {
-		TregexPattern p = TregexPattern.compile(pattern);
-		TregexMatcher m = p.matcher(t);
-
-		List<String> tokens = Arrays.asList(annotations.get(SFConstants.TOKENS).split(" "));
-		Set<Integer> places = new HashSet<Integer>();
-		int numLoops = 100;
-		FIND: while(m.find()) {
-			Tree match = m.getMatch();
-			int numInnerLoops = 100;
-			for(Tree node : match.getLeaves()) {
-				String name = node.value();
-				places.add(tokens.indexOf(name));
-				
-				// For some reason, we are getting an infinite loop on
-				// sentence ID 8299442. For now, just limit the number of
-				// loops to something reasonable.
-				numInnerLoops--;
-				if ( numInnerLoops == 0 ) break;
+		try {
+			TregexPattern p = TregexPattern.compile(pattern);
+			TregexMatcher m = p.matcher(t);
+	
+			List<String> tokens = Arrays.asList(annotations.get(SFConstants.TOKENS).split(" "));
+			Set<Integer> places = new HashSet<Integer>();
+			int numLoops = 100;
+			FIND: while(m.find()) {
+				Tree match = m.getMatch();
+				int numInnerLoops = 100;
+				for(Tree node : match.getLeaves()) {
+					String name = node.value();
+					places.add(tokens.indexOf(name));
+					
+					// For some reason, we are getting an infinite loop on
+					// sentence ID 8299442. For now, just limit the number of
+					// loops to something reasonable.
+					numInnerLoops--;
+					if ( numInnerLoops == 0 ) break;
+				}
+				numLoops--;
+				if ( numLoops == 0 ) break;
 			}
-			numLoops--;
-			if ( numLoops == 0 ) break;
-		}
-		
-		HashSet<String> placesSet = new HashSet<String>();
-		for(Integer placeIndex : places) {
-			if(placeIndex >= 0) {
-				String place = tokens.get(placeIndex);
-				if(annotations.get(SFConstants.STANFORDNER).split(" ")[placeIndex].equals("LOCATION"))
-					placesSet.add(place);
-				Collection<CorefMention> corefs = coref.inRange(placeIndex, placeIndex);
-				if(corefs.size() > 0) {
-					CorefEntity entity = corefs.iterator().next().entity;
-					if(entity.nerType == NerType.LOCATION)
-						placesSet.add(entity.repMention.entity.fullName);
-					else if(place.contains("-based")) {
-						place = place.replace("-based", "");
+			
+			HashSet<String> placesSet = new HashSet<String>();
+			for(Integer placeIndex : places) {
+				if(placeIndex >= 0) {
+					String place = tokens.get(placeIndex);
+					if(annotations.get(SFConstants.STANFORDNER).split(" ")[placeIndex].equals("LOCATION"))
 						placesSet.add(place);
+					Collection<CorefMention> corefs = coref.inRange(placeIndex, placeIndex);
+					if(corefs.size() > 0) {
+						CorefEntity entity = corefs.iterator().next().entity;
+						if(entity.nerType == NerType.LOCATION)
+							placesSet.add(entity.repMention.entity.fullName);
+						else if(place.contains("-based")) {
+							place = place.replace("-based", "");
+							placesSet.add(place);
+						}
 					}
 				}
 			}
+			
+			ArrayList<String> retList = new ArrayList<String>();
+			retList.addAll(placesSet);
+			return retList;
+		} catch ( Exception ex ) {
+			// TODO: this is a hack to catch PatternSyntaxExceptions, which can
+			// sometimes happen if a bad pattern is built.
+			// This happened in query set training-2010 in split number 5.
+			System.err.println("================== ERROR getting match names ===================");
+			ex.printStackTrace();
+			return Collections.emptyList();
 		}
-		
-		ArrayList<String> retList = new ArrayList<String>();
-		retList.addAll(placesSet);
-		return retList;
 	}
 	
 }
